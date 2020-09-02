beb76e2b55a97c4a7ddf89934aefdc5e3a5b85f3
bdb2381 <brad.beggs@gmail.com>
2020-09-02 16:59:39 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your code here
+class Breakfast {
+  constructor(food, drink){
+    this.food  =  food 
+    this.drink =  drink
+  }
+
+} // end class  breakfast
+
+
+class Lunch{
+  constructor(salad, soup, drink){
+    this.salad =  salad
+    this.soup = soup 
+    this.drink =  drink
+  }
+
+} //end class lunch
+
+
+class Dinner{
+  constructor(salad, soup, entree, _dessert){
+    this.salad =  salad
+    this.soup =  soup
+    this.entree =  entree
+    this._dessert =  _dessert
+  }
+
+
+
+} // end class dinner 
